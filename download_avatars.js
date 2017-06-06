e971dd7f0db9a0bb9633db1aa174b87b254ef3b6
caitlinquon <caitlin_quon@hotmail.com>
2017-06-06 05:52:03 +0000

    fixed lint errors

--- download_avatars.js
+++ download_avatars.js
@@ -4,20 +4,20 @@ var GITHUB_USER = 'caitlinquon';
 var GITHUB_TOKEN = '9a074496dee278a1861c904f3fc0c909f650a2fa';
 var owner = process.argv[2];
 var repo = process.argv[3];
-
+//cb helps return asynchoronous results of getRepoContributors
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
     url: requestURL,
     headers: {
-      'User-Agent' : 'caitlinquon'
-    }, 
-    json: true 
-  }
+      'User-Agent': 'caitlinquon'
+    },
+    json: true
+  };
   request.get(options, function(err, response, body){
     cb(err, body);
-  })
-};
+  });
+}
 
 function downloadImageByURL(url, filePath) {
   request(url)
@@ -25,11 +25,11 @@ function downloadImageByURL(url, filePath) {
 }
 
 getRepoContributors(owner, repo, function(err, body) {
- body.forEach(function(item, index, array){
-  downloadImageByURL(item.avatar_url, './avatars/' + item.login + '.jpg')
-  })
+  body.forEach(function(item, index, array){
+    downloadImageByURL(item.avatar_url, './avatars/' + item.login + '.jpg');
+  });
   
-})
+});
 
 
 
