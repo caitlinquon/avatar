49733a81941622ce115e9b17a218bff7526c89d7
caitlinquon <caitlin_quon@hotmail.com>
2017-06-06 02:51:52 +0000

    printed each avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -2,14 +2,25 @@ var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = 'caitlinquon';
 var GITHUB_TOKEN = '9a074496dee278a1861c904f3fc0c909f650a2fa';
-var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.come/repos/' + 'jquery' + '/' + 'jquery' + '/contributors';
+var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/jquery/jquery/contributors';
 
 function getRepoContributors(repoOwner, repoName, cb){
- 
-}
+  var options = {
+    url: requestURL, 
+    headers: {
+      'user-agent': 'LHL Project'
+    }
+  };
+  request.get(options, cb);
+};
+
+
 getRepoContributors("jquery", "jquery", function(err, result){
-  if(err) throw err;
-  console.log("Errors:", err);
-  console.log("Result :", result);
+  var emptyArray = [];
+  var data = JSON.parse(result.body);
+  data.forEach(function(item, index, array){
+    emptyArray.push(item.avatar_url);
+  });
+    
 });
 
