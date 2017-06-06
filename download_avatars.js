aed4d407fdb67df113a486e7c8c25382f918c268
caitlinquon <caitlin_quon@hotmail.com>
2017-06-06 01:27:42 +0000

    created a request URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
+var fs = require('fs');
+var GITHUB_USER = 'caitlinquon';
+var GITHUB_TOKEN = '9a074496dee278a1861c904f3fc0c909f650a2fa';
+var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.come/repos/' + 'jquery' + '/' + 'jquery' + '/contributors';
 
-console.log('Welcome to the Github Avatar Downlader!');
+function getRepoContributors(repoOwner, repoName, cb){
+ 
+}
+getRepoContributors("jquery", "jquery", function(err, result){
+  if(err) throw err;
+  console.log("Errors:", err);
+  console.log("Result :", result);
+});
+
